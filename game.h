05cacbe6c15442e522b35bedb167da6f13195caa
Lastlilith <lastlilith.2014@gmail.com>
2023-01-14 01:53:55 +0900

    Created controls and "eating" logic

--- game.h
+++ game.h
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <conio.h>
+#include <windows.h>
 
 using namespace std;
 
 bool gameOver;
-const int width = 30;
+const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
 enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
@@ -13,16 +15,16 @@ eDirection dir;
 void Setup() {
 	gameOver = false;
 	dir = STOP;
-	x = width / 2;
-	y = height / 2;
+	x = width / 2 -1;
+	y = height / 2 -1;
 	fruitX = rand() % width;
-	fruitX = rand() % height;
+	fruitY = rand() % height;
 	score = 0;
 }
 
 void Draw() {
 	system("cls"); //system("clear"); on Linux
-	for (int i = 0; i < width; i++) {
+	for (int i = 0; i < width + 1; i++) {
 		cout << "#";
 	}
 	cout << endl;
@@ -32,6 +34,12 @@ void Draw() {
 			if (j == 0 || j == width - 1) {
 				cout << "#";
 			}
+			if (i == y && j == x) {
+				cout << "0";
+			}
+			else if (i == fruitY && j == fruitX) {
+				cout << "F";
+			}
 			else {
 				cout << " ";
 			}
@@ -39,18 +47,59 @@ void Draw() {
 		cout << endl;
 	}
 
-	for (int i = 0; i < width; i++) {
+	for (int i = 0; i < width + 1; i++) {
 		cout << "#";
 	}
 	cout << endl;
+	cout << "Score: " << score << endl;
 }
 
 void Input() {
-
+	if (_kbhit()) {
+		switch (_getch()) {
+		case 'a':
+			dir = LEFT;
+			break;
+		case 'd':
+			dir = RIGHT;
+			break;
+		case 'w':
+			dir = UP;
+			break;
+		case 's':
+			dir = DOWN;
+			break;
+		case 'x':
+			gameOver = true;
+			break;
+		}
+	}
 }
 
 void Logic() {
+	switch (dir) {
+	case LEFT:
+		x--;
+		break;
+	case RIGHT:
+		x++;
+		break;
+	case UP:
+		y--;
+		break;
+	case DOWN:
+		y++;
+		break;
+	}
 
+	if (x > width || x < 0 || y > height || y < 0) {
+		gameOver = true;
+	}
+	if (x == fruitX && y == fruitY) {
+		score += 10;
+		fruitX = rand() % width;
+		fruitY = rand() % height;
+	}
 }
 
 
@@ -61,6 +110,7 @@ int main() {
 		Draw();
 		Input();
 		Logic();
+		Sleep(50);
 	}
 	return 0;
 }
