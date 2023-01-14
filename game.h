7e6a6b8d4f71a85f330fab25c8be592e493444bc
Lastlilith <lastlilith.2014@gmail.com>
2023-01-14 13:42:11 +0900

    Added tail and fixed several bugs

--- game.h
+++ game.h
@@ -8,6 +8,8 @@ bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTail;
 enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
 eDirection dir;
 
@@ -41,7 +43,16 @@ void Draw() {
 				cout << "F";
 			}
 			else {
-				cout << " ";
+				bool print = false;
+				for (int k = 0; k < nTail; k++) {
+					if (tailX[k] == j && tailY[k] == i) {
+						print = true;
+						cout << "o";
+					}
+				}
+				if (!print) {
+					cout << " ";
+				}
 			}
 		}
 		cout << endl;
@@ -77,6 +88,21 @@ void Input() {
 }
 
 void Logic() {
+	int prevX = tailX[0];
+	int prevY = tailY[0];
+	int prev2X, prev2Y;
+	tailX[0] = x;
+	tailY[0] = y;
+
+	for (int i = 1; i < nTail; i++) {
+		prev2X = tailX[i];
+		prev2Y = tailY[i];
+		tailX[i] = prevX;
+		tailY[i] = prevY;
+		prevX = prev2X;
+		prevY = prevY;
+	}
+
 	switch (dir) {
 	case LEFT:
 		x--;
@@ -92,13 +118,34 @@ void Logic() {
 		break;
 	}
 
-	if (x > width || x < 0 || y > height || y < 0) {
-		gameOver = true;
+	//if (x > width || x < 0 || y > height || y < 0) {
+		//gameOver = true;}   --> THIS IS USED IF YOU WANT TO KILL SNAKE WHEN TOUCHING THE BORDER OF THE MAP
+
+	if (x >= width -1) {
+		x = 0;
 	}
+	else if (x < 0) {
+		x = width - 2;
+	}
+
+	if (y >= height) {
+		y = 0;
+	}
+	else if (y < 0) {
+		y = height - 1;
+	}
+
+	for (int i = 0; i < nTail; i++) {
+		if (tailX[i] == x && tailY[i] == y) {
+			gameOver = true;
+		}
+	}
+
 	if (x == fruitX && y == fruitY) {
 		score += 10;
 		fruitX = rand() % width;
 		fruitY = rand() % height;
+		nTail++;
 	}
 }
 
